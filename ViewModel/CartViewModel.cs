df63ccf4691469e529b6beda91ee877b0208b316
Toan Tran Cong <22110248@student.hcmute.edu.vn>
2024-03-25 15:10:10 +0700

    Add project files.

---
+++ ViewModel/CartViewModel.cs
@@ -0,0 +1,43 @@
+﻿using Exchange_App.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Exchange_App.ViewModel
+{
+    public class CartViewModel :BaseViewModel
+    {
+        private User _currentUser;
+
+        private List<object> _cartItems;
+
+        public CartViewModel(User user)
+        {
+            CurrentUser = user;
+
+            var custQuery = from cart in CurrentUser.CartItems
+                            join product in DataProvider.Ins.DB.Products
+                            on cart.ProductID equals product.ProductID
+                            select new {
+                                ProductID = product.ProductID,
+                                Original_price = product.Original_price,
+                                Sell_price = product.Sell_price,
+                                ProductName = product.ProductName,
+                                PreviewImage = product.GetPreviewImage,
+                                Category = product.Category,
+                                Product_Quantity = product.Quantity,
+                                CartItemsID = cart.CartItemsID,
+                                CartQuantity = cart.Quantity
+                            };
+                    MessageBox.Show("Cart Items: " + custQuery.Count().ToString() + " items in cart.");
+            CartItems = custQuery.ToList<object>();
+        }
+
+        public User CurrentUser { get => _currentUser; set => _currentUser=value; }
+        public List<object> CartItems { get => _cartItems; set => _cartItems=value; }
+    }
+}
---
+++ ViewModel/ProductDetailsViewModel.cs
@@ -0,0 +1,75 @@
+﻿using Exchange_App.Model;
+using Microsoft.Expression.Interactivity.Media;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace Exchange_App.ViewModel
+{
+    internal class ProductDetailsViewModel
+    {
+        private Product _product;
+        private User _currentUser;
+
+
+
+        public ProductDetailsViewModel(Product product, User currentUser)
+        {
+            _product = product;
+            _currentUser = currentUser;
+        }
+
+        // extract all prop of _product to property
+
+        public int ProductID => _product.ProductID;
+
+        public string ProductName => _product.ProductName;
+        public double Original_price => _product.Original_price;
+        public double Sell_price => _product.Sell_price;
+        public string Info_des => _product.Info_des;
+
+        public string Status_Des => _product.Status_des;
+        //
+
+
+        #region Commands
+        private readonly ICommand _addProductToCartCommand;
+
+        public ICommand AddProductToCartCommand {
+            get
+            {
+                return _addProductToCartCommand ?? (new RelayCommand<int>((p) => { return true; }, (p) => {
+                    // Check if the product is already in the cart
+                    int productID = p;
+                    if (_currentUser.CartItems.Any(x => x.ProductID == productID))
+                    {
+                        // increase quanitty
+                        _currentUser.CartItems.First(x => x.ProductID == productID).Quantity++;
+                    } else
+                    {
+                        _currentUser.CartItems.Add(new CartItem
+                        {
+                            ProductID = productID,
+                            Quantity = 1,
+                            User = _currentUser
+                        });
+                    }
+                    
+                    //submit changes to the database ado.net
+                    DataProvider.Ins.DB.SaveChanges();
+
+                })); 
+            }
+        }
+
+
+        #endregion
+
+    }
+
+
+}
